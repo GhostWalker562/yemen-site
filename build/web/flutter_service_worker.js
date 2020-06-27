a17e95b0528dfcfd91804a9a9d79f45f49541ee9
Moon <43276017+GhostWalker562@users.noreply.github.com>
2020-06-26 18:58:21 -0700

    Deployed and hosted on Firebase

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,17 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "f299bcaa90aff2c5a6fbe3d14283b48a",
+"assets/NOTICES": "2e8ce10f0c0fe6f24c4effa990d1b591",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "77cb2242d0b5977e6eb38479f45a1d12",
 "/": "77cb2242d0b5977e6eb38479f45a1d12",
-"main.dart.js": "76a5726e52da71f92354e4aec263c44f",
+"main.dart.js": "89908096f975403217d32661c7506cff",
 "manifest.json": "c9b127d47417e116f8aad51f872f125d"
 };
 
